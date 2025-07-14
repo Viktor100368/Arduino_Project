8a9aa34ccee15293b335ae73c0fd295e86b08e40
Viktor100368 <Vbur100368@gmail.com>
2025-07-14 22:30:28 +0400

    aded  function stoping() and stopCar()

--- sketch_tachca/sketch_tachca.ino
+++ sketch_tachca/sketch_tachca.ino
@@ -13,12 +13,20 @@ int rightVal;//длительность сигнала используемая 
 int *p_midle;
 int *p_leftVal;
 int *p_rightVal;
-int forvardOrBack;
-int *p_forvardOrBack;
+bool forvardOrBack = true;
+bool *p_forvardOrBack;
+bool prevValueForvardOrBack = 1002;
+bool *p_prevValueForvardOrBack;
+bool togleEvent = false;
+bool *p_togleEvent;
 unsigned long time = 0;
 int chanel[] ={2,3,4,5,6,7};
+
 int duration[count];
 int countTimer;
+int stopingTimer;
+
+
 
 
 void setup() {
@@ -28,7 +36,9 @@ void setup() {
   p_leftVal = &leftVal;
   p_rightVal = &rightVal;
   p_midle = &midle;
-  P_forvardOrBack = &forvardOrBack;
+  p_forvardOrBack = &forvardOrBack;
+  p_togleEvent = &togleEvent;
+  p_prevValueForvardOrBack = &prevValueForvardOrBack;
   
   for(int i=0; i<count; i++){
     pinMode(chanel[i],INPUT);
@@ -45,12 +55,20 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   if(countTimer >= 2){
-    setSpeedAndDirection(duration[2], duration[3], p_leftVal, p_rightVal, p_midle);
+    setSpeedAndDirection(duration[2], duration[3], p_leftVal, p_rightVal);
   }
+  if(stopingTimer >=3){
+    stoping(duration[5], p_forvardOrBack, p_midle, p_prevStateForvard, p_togleEvent);
+  }
+  //управление двигателями
   leftPwm = map(leftVal,1000,2000,0,100);
   rightPwm = map(rightVal,1000,2000,0,100);
   leftPwm = constrain(leftPwm,0,95);
   rightPwm = constrain(rightPwm,0,95);
+  //проверка смены направления движения
+  if(togleEvnet == true){
+    stopCar();
+  }
   Serial.println("==========================");
   Serial.print("leftPwm = ");
   Serial.println(leftPwm);
@@ -76,9 +94,10 @@ void timerInterupt(){
     duration[i]=pulseIn(chanel[i],HIGH);
   }
   countTimer++;
+  stopingTimer++;
   MsTimer2::start();
 }
-void setSpeedAndDirection(int valSpeed, int valDirection, int *p_lV, int *p_rV,int *p_midle){
+void setSpeedAndDirection(int valSpeed, int valDirection, int *p_lV, int *p_rV){
   MsTimer2::stop();
   if((valDirection > 1300)&&(valDirection < 1480)){
     *p_lV = valSpeed-150;//коэффициент пересчета
@@ -112,6 +131,20 @@ void setSpeedAndDirection(int valSpeed, int valDirection, int *p_lV, int *p_rV,i
   MsTimer2::start();
 }
 
-void stoping(int valForOrBack){
-
+void stoping(int valForOrBack, bool *p_forvardOrBack, int *p_midle, int *p_prevValueForvardOrBack, bool *p_togleEvent)){
+  MsTimer2::stop();
+  if(valForOrBack < *p_midle){
+    *p_forvardOrBack = true;
+  }else if(valForOrBack > *p_midle){
+    *p_forvardOrBack = false;
+  }
+  if(valForOrBack != *p_prevValueForvardOrBack){
+    *p_togleEvent = true;
+  }else 
+    *p_togleEvent = false;
+  stopingTimer = 0;
+  MsTimer2::start();
+}
+void stopCar(){
+  
 }
